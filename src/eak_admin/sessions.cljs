092e564a2de4c30d33038ea2782ec3f9c22f8d43
Joe Dytrych <me@jdytrych.co.uk>
2014-05-27 12:05:38 +0100

    Show session info in session buttons

--- src/eak_admin/sessions.cljs
+++ src/eak_admin/sessions.cljs
@@ -25,6 +25,14 @@
    (nil? (:children session)) false
    true (apply ors (map #(has? k v %) (:children session)))))
 
+(defn count-walk [k v session]
+  (if (nil? session)
+    0
+    (let [b (if (= v (k session)) 1 0)]
+      (if (nil? (:children session))
+        b
+        (apply + b (map #(count-walk k v %) (:children session)))))))
+
 (defn get-color-class [session]
   (if (has? :type "show-form" session) "success" "danger"))
 
@@ -36,10 +44,20 @@
 
 (defn format-duration [n] (str (pad (int (/ n 60)) 0 2) ":" (pad (int (rem n 60)) 0 2)))
 
+(defn prop [title value]
+  (dom/div #js {:className "prop col-xs-4"}
+    (dom/h6 nil title)
+    (dom/div #js {:className "num"} value)))
+
 (defn session-small [session]
   (dom/div #js {:className "col-md-3 col-sm-4 col-xs-6 session-small"}
     (dom/a #js {:className (str "btn btn-block btn-" (get-color-class session)) :href (str "#/sessions/" (:id session))}
-      (dom/div #js {:className "duration"} (format-duration (:duration session))))))
+      (dom/div #js {:className "duration"} (format-duration (:duration session)))
+      (dom/div #js {:className ""}
+        (prop "Levels" (count-walk :type "level" session))
+        (prop "Kittens" (count-walk :type "kitten" session))
+        (prop "Deaths" (count-walk :type "death" session)))
+      (dom/div #js {:className "row"}))))
 
 (defn session-list [{:keys [limit offset]} owner]
   (reify
